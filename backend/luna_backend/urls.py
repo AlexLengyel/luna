524fe3517951ac3ce95a2b71c96772238dba1943
cbarcan <catalin.barcan@ymail.com>
2021-06-28 13:40:25 +0200

    server setup - backend adding some urls

--- backend/luna_backend/urls.py
+++ backend/luna_backend/urls.py
@@ -18,7 +18,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt import views as jwt_views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="RCM API",
+        default_version='v1.0',
+        description="Recipe and cookbook management API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="catalin.barcan@ymail.com"),
+        license=openapi.License(name="ACB License"),
+    ),
+    public=True,  # Set to False restrict access to protected endpoints
+    permission_classes=(permissions.AllowAny,),  # Permissions for docs access
+)
 
 urlpatterns = [
                   path(
@@ -40,10 +56,10 @@ urlpatterns = [
                       jwt_views.TokenVerifyView.as_view(),
                       name='token_refresh'
                   ),
-                  # path(
-                  #     'backend/api/docs/',
-                  #     schema_view.with_ui('swagger', cache_timeout=0),
-                  #     name='schema-swagger-ui'),
+                  path(
+                      'backend/api/docs/',
+                      schema_view.with_ui('swagger', cache_timeout=0),
+                      name='schema-swagger-ui'),
               ] + static(settings.STATIC_URL,
                          document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
