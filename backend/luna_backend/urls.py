577889c843dddecd16374d97a8670adb75100823
cbarcan <catalin.barcan@ymail.com>
2021-06-28 13:36:12 +0200

    server setup - backend adding some urls

--- backend/luna_backend/urls.py
+++ backend/luna_backend/urls.py
@@ -17,10 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
@@ -43,10 +40,10 @@ urlpatterns = [
                       jwt_views.TokenVerifyView.as_view(),
                       name='token_refresh'
                   ),
-                  path(
-                      'backend/api/docs/',
-                      schema_view.with_ui('swagger', cache_timeout=0),
-                      name='schema-swagger-ui'),
+                  # path(
+                  #     'backend/api/docs/',
+                  #     schema_view.with_ui('swagger', cache_timeout=0),
+                  #     name='schema-swagger-ui'),
               ] + static(settings.STATIC_URL,
                          document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
