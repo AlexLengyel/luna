fea7ca49723643ab59b629cbcdb8475d482313f1
15jpr <l.furck@gmail.com>
2021-06-30 14:15:58 +0200

    rewrite it to use grid

--- frontend/src/Pages/CreateRestaurant.jsx
+++ frontend/src/Pages/CreateRestaurant.jsx
@@ -4,14 +4,15 @@ import Title from "../Components/BaseTitle";
 import { BaseInput } from "../Components/BaseInput";
 import {useState} from 'react';
 import { CreateRestaurantWrapper } from "../Style/container";
+import { useForm } from "react-hook-form";
 
 
-
-const InputWrapper = styled.div `
-    display: grid;  
-    grid-template-rows: repeat(8, 1fr);
-    height: 60vh;
-    width: 80vw;
+const InputSection = styled.div `
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);  
+    grid-template-rows: repeat(4, 1fr);
+    column-gap: 2vw;
+    align-items: flex-end;
     margin-top: 5vh;
 `
 
@@ -31,6 +32,7 @@ const UploadButton = styled(Button)`
     width: 14vw;
     min-width: 100px;
     border-radius: 28px;
+    margin-bottom: 20px;
 `
 
 const SelectInput = styled.select`
@@ -42,34 +44,12 @@ const SelectInput = styled.select`
     color: ${props => props.theme.lightSilver};
 `
 
-const InputLabel = styled.label`
+const InputWrapper = styled.label`
     display: flex;
     flex-direction: column;
-    align-items: space-between;
+    align-content: bottom;
 `
 
-const BasicWrapper = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 2vw;
-`
-
-const AdressWrapper = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 2vw;
-`
-
-const ContactWrapper = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 2vw;
-`
-const DetailWrapper = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 2vw;
-`
 const SectionTitle = styled.p`
     padding-bottom: 5px;
 `
@@ -97,81 +77,80 @@ const CreateRestaurant = () => {
 
 	};
 
-    
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
     return(
         <CreateRestaurantWrapper>
             <Title titlename="CREATE NEW RESTAURANT" linelength="200px" height="10vh"/>
-            <InputWrapper>
-                <SectionTitle>Basic</SectionTitle>
-                <BasicWrapper>
-                    <InputLabel><InputTitle>Name *</InputTitle>
-                        <BaseInput placeholder='' required/>
-                    </InputLabel>
-                    <InputLabel><InputTitle>Category *</InputTitle>
-                        <SelectInput required>
-                            <option value="" selected disabled hidden>Select a value...</option>
-                            <option value="Category 1">Category 1</option>
-                            <option value="Category 2">Category 2</option>
-                            <option value="Category 3">Category 3</option>
-                        </SelectInput>
-                    </InputLabel>
-                    <InputLabel><InputTitle>Country *</InputTitle>
-                        <SelectInput required>
-                            <option value="" selected disabled hidden>Select a value...</option>
-                            <option value="Switzerland">Switzerland</option>
-                            <option value="Austria">Austria</option>
-                            <option value="Germany">Germany</option>
-                            <option value="Italy">Italy</option>
-                        </SelectInput>                
-                    </InputLabel>
-                </BasicWrapper>
-                <SectionTitle>Address</SectionTitle>
-                <AdressWrapper>
-                    <InputLabel><InputTitle>Street *</InputTitle>
-                        <BaseInput placeholder='' required/>
-                    </InputLabel>
-                    <InputLabel><InputTitle>City *</InputTitle>
-                        <BaseInput placeholder='' required/>
-                    </InputLabel>
-                    <InputLabel><InputTitle>Zip</InputTitle>
-                        <BaseInput placeholder=''/>
-                    </InputLabel>
-                </AdressWrapper>
-                <SectionTitle>Contact</SectionTitle>
-                <ContactWrapper>
-                    <InputLabel><InputTitle>Website</InputTitle>
-                        <BaseInput placeholder=''/>
-                    </InputLabel>
-                    <InputLabel><InputTitle>Phone *</InputTitle>
-                        <BaseInput placeholder='' required/>
-                    </InputLabel>
-                    <InputLabel><InputTitle>Email</InputTitle>
-                        <BaseInput placeholder=''/>
-                    </InputLabel>
-                </ContactWrapper>
-                <SectionTitle>Details</SectionTitle>
-                <DetailWrapper>
-                    <InputLabel><InputTitle>Opening hours *</InputTitle>
-                        <BaseInput placeholder='' required/>
-                    </InputLabel>
-                    <InputLabel><InputTitle>Price level</InputTitle>
-                        <SelectInput>
-                            <option value="" selected disabled hidden>Select a value...</option>
-                            <option value="$">$</option>
-                            <option value="$$">$$</option>
-                            <option value="$$$">$$$</option>
-                        </SelectInput>
-                    </InputLabel>
-                    <InputLabel><InputTitle>Image</InputTitle>                       
-                        <UploadButton>
-                            <UploadLabel>
-                                <input hidden type="file" accept="image/png, image/jpg" onChange={changeHandler}/>                    
-                                CHOOSE A FILE...
-                            </UploadLabel>
-                        </UploadButton>
-                    </InputLabel>
-                </DetailWrapper>
-            </InputWrapper>
+            <InputSection>
+                <InputWrapper>
+                    <SectionTitle>Basic</SectionTitle>
+                    <InputTitle>Name *</InputTitle>
+                    <BaseInput placeholder='' required/>
+                </InputWrapper>
+                <InputWrapper>
+                    <InputTitle>Category *</InputTitle>
+                    <SelectInput required>
+                        <option value="" selected disabled hidden>Select a value...</option>
+                        <option value="Category 1">Category 1</option>
+                        <option value="Category 2">Category 2</option>
+                        <option value="Category 3">Category 3</option>
+                    </SelectInput>
+                </InputWrapper>
+                <InputWrapper>
+                    <InputTitle>Country *</InputTitle>
+                    <SelectInput required>
+                        <option value="" selected disabled hidden>Select a value...</option>
+                        <option value="Switzerland">Switzerland</option>
+                        <option value="Austria">Austria</option>
+                        <option value="Germany">Germany</option>
+                        <option value="Italy">Italy</option>
+                    </SelectInput>                
+                </InputWrapper>
+                <InputWrapper> 
+                    <SectionTitle>Address</SectionTitle>
+                    <InputTitle>Street *</InputTitle>
+                    <BaseInput placeholder='' required/>
+                </InputWrapper>
+                <InputWrapper><InputTitle>City *</InputTitle>
+                    <BaseInput placeholder='' required/>
+                </InputWrapper>
+                <InputWrapper><InputTitle>Zip</InputTitle>
+                    <BaseInput placeholder=''/>
+                </InputWrapper>
+                <InputWrapper>
+                    <SectionTitle>Contact</SectionTitle>
+                    <InputTitle>Website</InputTitle>
+                    <BaseInput placeholder=''/>
+                </InputWrapper>
+                <InputWrapper><InputTitle>Phone *</InputTitle>
+                    <BaseInput placeholder='' required/>
+                </InputWrapper>
+                <InputWrapper><InputTitle>Email</InputTitle>
+                    <BaseInput placeholder=''/>
+                </InputWrapper>
+                <InputWrapper>
+                    <SectionTitle>Details</SectionTitle>
+                    <InputTitle>Opening hours *</InputTitle>
+                    <BaseInput placeholder='' required/>
+                </InputWrapper>
+                <InputWrapper><InputTitle>Price level</InputTitle>
+                    <SelectInput>
+                        <option value="" selected disabled hidden>Select a value...</option>
+                        <option value="$">$</option>
+                        <option value="$$">$$</option>
+                        <option value="$$$">$$$</option>
+                    </SelectInput>
+                </InputWrapper>
+                <InputWrapper>
+                    <InputTitle>Image</InputTitle>                       
+                    <UploadButton>
+                        <UploadLabel>
+                            <input hidden type="file" accept="image/png, image/jpg" onChange={changeHandler}/>                    
+                            CHOOSE A FILE...
+                        </UploadLabel>
+                    </UploadButton>
+                </InputWrapper>    
+            </InputSection>
             <ButtonWrapper>
                 <SubmitButton onclick={submitHandler}>
                     Submit
